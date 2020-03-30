42502a9d668778a4122b8e7538c8dd9b5f1056f0
Shaetrun <pyrodragon8@users.noreply.github.com>
2020-03-30 20:41:18 +1300

    throwins

--- CPU1_FLASH/subdir_rules.mk
+++ CPU1_FLASH/subdir_rules.mk
@@ -5,7 +5,7 @@
 SHELL = cmd.exe
 
 # Each subdirectory must supply rules for building sources it contributes
-Example_2837xDSpi_FFDLB.obj: C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/examples/cpu1/spi_loopback/cpu01/Example_2837xDSpi_FFDLB.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C2000 Compiler'
 	"C:/ti/ccs920/ccs/tools/compiler/ti-cgt-c2000_18.12.3.LTS/bin/cl2000" -v28 -ml -mt --vcu_support=vcu2 --tmu_support=tmu0 --cla_support=cla1 --float_support=fpu32 --include_path="C:/ti/ccs920/ccs/tools/compiler/ti-cgt-c2000_18.12.3.LTS/include" --include_path="C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/headers/include" --include_path="C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/common/include" -g --define=CPU1 --define=_FLASH --display_error_number --diag_suppress=10063 --diag_warning=225 --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
--- CPU1_FLASH/subdir_vars.mk
+++ CPU1_FLASH/subdir_vars.mk
@@ -10,7 +10,7 @@ C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/common/source/F2837xD
 C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/common/source/F2837xD_usDelay.asm 
 
 C_SRCS += \
-C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/examples/cpu1/spi_loopback/cpu01/Example_2837xDSpi_FFDLB.c \
+../Example_2837xDSpi_FFDLB.c \
 C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/common/source/F2837xD_DefaultISR.c \
 C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/headers/source/F2837xD_GlobalVariableDefs.c \
 C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/common/source/F2837xD_Gpio.c \
@@ -77,7 +77,7 @@ ASM_DEPS__QUOTED += \
 "F2837xD_usDelay.d" 
 
 C_SRCS__QUOTED += \
-"C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/examples/cpu1/spi_loopback/cpu01/Example_2837xDSpi_FFDLB.c" \
+"../Example_2837xDSpi_FFDLB.c" \
 "C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/common/source/F2837xD_DefaultISR.c" \
 "C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/headers/source/F2837xD_GlobalVariableDefs.c" \
 "C:/ti/C2000Ware_2_00_00_03_Software/device_support/f2837xd/common/source/F2837xD_Gpio.c" \
